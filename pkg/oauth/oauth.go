22fb33dbd3539a29981ae8c2cfbba39e34e79f9b
Amine Bizid <amine.bizid@engie.com>
2019-05-27 12:25:25 +0200

    token

--- pkg/oauth/oauth.go
+++ pkg/oauth/oauth.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"strings"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/gorilla/sessions"
 )
 
@@ -50,6 +51,7 @@ func getRedirectIss(wellKnownURL, clientID, scopes, redirectURI string) string {
 		return ""
 	}
 	defer resp.Body.Close()
+
 	var wellKnown wellKnown
 	json.NewDecoder(resp.Body).Decode(&wellKnown)
 
@@ -62,17 +64,57 @@ func getRedirectIss(wellKnownURL, clientID, scopes, redirectURI string) string {
 		"&nonce=nonce"
 }
 
+func (p *Provider) redirect(res http.ResponseWriter) {
+	html := `
+		<html>
+			<head>
+				<script>
+					try {
+						x = location.hash.split('&');
+						y = x[0].split('=');
+						if (y[1] != '#access_token') {
+							location.replace("/sign-error?error=forbidden");
+						}
+						token = y[1]
+						console.log(token)
+						location.replace("/signin-token?token=" + token);
+					} catch(error) {
+						location.replace("/sign-oidc?error=" + error);
+					}
+				</script>
+			</head>
+		</html>`
+	res.Write([]byte(html))
+}
+
+func (p *Provider) recieveToken(req *http.Request) bool {
+	tokens, ok := req.URL.Query()["token"]
+	if !ok || len(tokens[0]) < 1 {
+		log.Error("Url Param 'token' is missing")
+		return false
+	}
+	return true
+}
+
 // Check if Authenticated
 func (p *Provider) Check(res http.ResponseWriter, req *http.Request) bool {
-	session, _ := p.store.Get(req, p.cookieName)
+
+	if req.RequestURI == "/signin-oidc" {
+		p.redirect(res)
+		return false
+	}
+
+	if strings.HasPrefix(req.RequestURI, "/signin-token") {
+		return p.recieveToken(req)
+	}
 
 	userAgent := req.Header.Get("User-Agent")
 	if strings.HasPrefix(userAgent, "Mozilla") {
+		session, _ := p.store.Get(req, p.cookieName)
 		// Ok if already authenticated
 		if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
 			http.Redirect(res, req, p.redirectIss, 302)
 		}
-
 	}
 
 	return false
